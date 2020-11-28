16eaf2f9c14a15611a27ed8aed137417a325d68f
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-29 00:17:27 +0300

    export secret key

--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,2 @@
 NEWS_API_KEY = "f704406e04b249f7b6e18849f7bb114c"
+SECRET_KEY='chucknorris'
---
+++ start.sh
@@ -0,0 +1,2 @@
+export NEWS_API_KEY = "f704406e04b249f7b6e18849f7bb114c"
+export SECRET_KEY='chucknorris'
