0c11e63fd02bf42f67da89ada1ea199ed6e0c2ce
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-28 16:47:38 +0300

    importing files from other files to request file

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+class News:
+
+    '''
+    News class to define Objects
+    '''
+
+    def __init__(self,name,description,url):
+
+        self.name = name
+        self.description = description
+        self.url = url
