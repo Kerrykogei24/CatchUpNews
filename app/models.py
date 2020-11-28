9dcc8273bea4186f3cb519acb5ae8ebb779a2c52
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-28 23:29:18 +0300

    add bootstrap and some seting config

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 class Sources:
 
     '''
-    News class to define Objects
+    source class to define Objects
     '''
 
     def __init__(self,id,name,description,url):
@@ -9,3 +9,15 @@ class Sources:
         self.name = name
         self.description = description
         self.url = url
+
+class Headlines:
+    '''
+    class that define instance of headlines
+    '''
+     def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+    HEAD_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
 
 
@@ -25,3 +29,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
