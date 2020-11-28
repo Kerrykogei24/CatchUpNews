d53f5f588bfb4d2d2b754e6bbb03d2c0391cd8c5
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-28 16:40:29 +0300

    testing news class

---
+++ app/news_test.py
@@ -0,0 +1,23 @@
+import unittest
+from models import news
+
+News = news.News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test behaviour of Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = News('news','headlines','sports')
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+
+if __name__ == "__main__":
+    unittest.main()
